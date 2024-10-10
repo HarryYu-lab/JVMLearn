e51a182e91d6c559927e934240afc73dda61866b
unknown <1170327086@qq.com>
2024-10-10 23:29:19 +0800

    ch02 down

--- ch02/Main.py
+++ ch02/Main.py
@@ -7,8 +7,10 @@
 **************************************
 """
 
+import os
 import argparse
-from ch01.Cmd import Cmd
+from ch02.Cmd import Cmd
+from ch02.classpath import Classpath
 
 
 def main(input_args):
@@ -31,12 +33,27 @@ def main(input_args):
 
 
 def start_JVM(cmd):
-    cmd.print_classpath()
+    # 解析类路径
+
+    class_path = Classpath.Classpath.parse(cmd.XjreOption, cmd.cpOption)
+    # 打印命令行参数
+    print("classpath: {0} class: {1} args: {2}".format(class_path, cmd.class_name, cmd.args))
+
+    # 读取主类数据
+    class_name = cmd.class_name.replace(".", "/")
+
+    class_data, _, error = class_path.read_class(class_name)
+    if error:
+        print("Could not find or load main class {0}\n".format(cmd.class_name))
+        exit(0)
+
+    # 打印class里面的数据信息
+    print("class data: {0}".format([int(hex(d), 16) for d in class_data]))
 
 
 if __name__ == '__main__':
-    fake_argvs = ['-v']
-    main(fake_argvs)
+    Xjre_path = os.path.join(os.environ.get("JAVA_HOME"), "jre")
 
-    fake_argvs = ['--cp', 'foo/bar', 'MyApp', 'arg1', 'arg2']
-    main(fake_argvs)
+    # 指定-Xjre选项和类名
+    fake_args = ['--Xjre', Xjre_path, 'java.lang.Object']
+    main(fake_args)
--- ch02/classpath/Entry.py
+++ ch02/classpath/Entry.py
@@ -7,16 +7,31 @@
 **************************************
 """
 from abc import ABCMeta, abstractmethod
-class Entry(ABCMeta):
+class Entry(metaclass=ABCMeta):
     # 路径分隔符
     path_list_separator = ";"
 
     # 寻找和加载class文件，接口方法
     @abstractmethod
-    def red_class(self, class_name):
+    def read_class(self, class_name):
         pass
 
+    # 根据参数常见不同类型的Entry实例
     @staticmethod
     def new_entry(path):
+        from ch02.classpath.CompositeEntry import CompositeEntry
+        from ch02.classpath.ZipEntry import ZipEntry
+        from ch02.classpath.DirEntry import DirEntry
+        from ch02.classpath.WildcardEntry import WildcardEntry
+
+        if Entry.path_list_separator in path:
+            return CompositeEntry(path)
+        elif path.endswith("*"):
+            return WildcardEntry(path)
+        elif path.endswith(".jar") or path.endswith(".JAR") or path.endswith(".zip") or path.endswith(".ZIP"):
+            return ZipEntry(path)
+        else:
+            return DirEntry(path)
+
 
 
